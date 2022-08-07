6f2606432614dc5d68d5f5bb43848dd560a86f53
Abhishek Pandey <ashp.pandey916@gmail.com>
2022-08-08 02:55:36 +0530

    Change database location

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def serve(path):
 def init_db():
     with app.app_context():
         db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
+        with app.open_resource('./database/schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
